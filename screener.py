01a2b74666ea10b2a82121a3406171d80e1c2e12
n3i7ik <naitiksharma4321@gmail.com>
2026-03-22 00:01:22 +0530

    Rename Nexara Stock Screener to Akcelara Stock Screener

--- screener.py
+++ screener.py
@@ -3,9 +3,9 @@ import pandas as pd
 import requests
 import random
 
-st.set_page_config(page_title="Nexara Stock Screener", layout="wide")
+st.set_page_config(page_title="Akcelara Stock Screener", layout="wide")
 st.image("logo.jpg", width=250)
-st.title("🇮🇳 Nexara Stock Screener")
+st.title("🇮🇳 Akcelara Stock Screener")
 st.caption("Filter NSE stocks by key fundamental metrics")
 
 HEADERS = {
